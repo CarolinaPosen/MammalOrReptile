1b7982bb2844f5d28910a688e5f98ffdeeb4ad85
Yauheni <curiosityopportunity@gmail.com>
2021-09-18 16:44:13 +0300

    Add determineClassOfOrganism method

--- src/by/itacademy/MammalOrReptile.java
+++ src/by/itacademy/MammalOrReptile.java
@@ -2,5 +2,16 @@ package by.itacademy;
 
 public class MammalOrReptile {
     public static void main(String[] args) {
+        System.out.println(determineClassOfOrganism(""));
+    }
+
+    private static String determineClassOfOrganism(String organism) {
+        String classOfOrganism = "";
+        if (organism == null || organism.equals("")) {
+            classOfOrganism = "incorrect input";
+        } else {
+            classOfOrganism = (true) ? "Mammal" : "Reptile";
+        }
+        return classOfOrganism;
     }
 }
